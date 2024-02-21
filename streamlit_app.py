e749694b38d483556baac99120aada5bcbaf0379
Jiarui Xu <christinexu@Jiaruis-MacBook-Pro.local>
2024-02-21 12:03:37 -0800

    update streamlit sidebar

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,6 @@
 # Code refactored from https://docs.streamlit.io/knowledge-base/tutorials/build-conversational-apps
 # pip install -r requirements.txt
 
-#TODO: move filters to side bar
-
 import streamlit as st
 import openai
 from openai import OpenAI
@@ -14,10 +12,40 @@ from io import BytesIO
 from input_image import read_image
 import json
 
-# create website sidebar
+# create website sidebar, with api_key input and filters/sort
 with st.sidebar:
     openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
     "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
+
+# Using a select form?
+    diets = [None, 'vegan', 'lactose free']
+    cuisines = [None, 'african', 'american', 'asian', 'creole and cajun', 
+                'english','french','greek','indian','italian',
+                'latin american','mediterranean','middle eastern',
+                'russian and ukranian','spanish',
+                'thai','turkish']
+    types = [None, 'breakfast and brunch', 'main dish', 'side dish', 'salad', 'baked goods']
+
+
+    with st.form('category_form', clear_on_submit=True):
+        st.selectbox('Select Dietary Restriction', diets, key='diet')
+        st.selectbox('Select Cuisines', cuisines,key='cuisine')
+        st.selectbox('Select Meal Type', types, key='type')
+
+        "---"
+        with st.expander('Extra requirements'):
+            requests = st.text_area("", placeholder='Enter extra requirements here...')
+
+        submitted = st.form_submit_button('Save inputs')
+        if submitted:
+            # TODO: merging the data based on submitted fields 
+            categorical = str(st.session_state['diet']) + str(st.session_state['cuisine']) + str(st.session_state['type'])
+            words = str(st.session_state['Extra requirements'])
+
+            # TODO: save into database for interaction
+            st.write(f'Current categorical : {categorical}')
+            st.write(f'Current additional comment: {words}')
+
 
 
 # create page header
@@ -36,7 +64,7 @@ st.caption(":shallow_pan_of_food: A cooking assistant powered by OpenAI LLM")
 
 #default on screen message
 if "messages" not in st.session_state:
-    st.session_state["messages"] = [{"role": "assistant", "content": "Please identify all the food ingredients in the image, response in json format with the list of items."}]
+    st.session_state["messages"] = [{"role": "assistant", "content": "Please upload an image so we can get started!"}]
 
 
 # TODO: do we need streamlit_option_menu?
@@ -70,7 +98,7 @@ if uploaded_file is not None:
 
         # response = requests.post("https://api.openai.com/v1/chat/completions", json=payload, headers=headers)
         response = read_image.ingredient_identifier(image_base64, 'streamlit', openai_api_key)
-        ing = json.loads(response['choices'][0]['message']['content'].strip('` \n').strip('json\n'))['ingredients']
+        ing = json.loads(response['choices'][0]['message']['content'].strip('` \n').strip('json\n'))['items']
         st.write(ing)
         
         # if response.status_code == 200:
@@ -80,36 +108,6 @@ if uploaded_file is not None:
 
 ##----------------------------------------------------
 
-################## Ask for ingredient editing ################
-# Using a select form?
-diets = [None, 'vegan', 'lactose free']
-cuisines = [None, 'african', 'american', 'asian', 'creole and cajun', 
-            'english','french','greek','indian','italian',
-            'latin american','mediterranean','middle eastern',
-            'russian and ukranian','spanish',
-            'thai','turkish']
-types = [None, 'breakfast and brunch', 'main dish', 'side dish', 'salad', 'baked goods']
-
-with st.form('caegory_form', clear_on_submit=True):
-    col1, col2, col3 = st.columns(3)
-    col1.selectbox('Select Dietary Restriction', diets, key='diet')
-    col2.selectbox('Select Cuisines', cuisines,key='cuisine')
-    col3.selectbox('Select Meal Type', types, key='type')
-
-    "---"
-    with st.expander('Extra requirements'):
-        requests = st.text_area("", placeholder='Enter extra requirements here...')
-
-    submitted = st.form_submit_button('Save inputs')
-    if submitted:
-
-        # TODO: merging the data based on submitted fields 
-        categorical = str(st.session_state['diet']) + str(st.session_state['cuisine']) + str(st.session_state['type'])
-        words = str(st.session_state['Extra requirements'])
-
-        # TODO: save into database for interaction
-        st.write(f'Current categorical : {categorical}')
-        st.write(f'Current additional comment: {words}')
 
                 
 ##----------------------------------------------------
@@ -125,6 +123,8 @@ with st.form('caegory_form', clear_on_submit=True):
 #################### 
 
 # # TODO: create separated menu? for the interactive guiding page
+
+
 
 for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg["content"])

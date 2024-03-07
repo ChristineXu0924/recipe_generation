bd983272c53419311628d133f55a05539771e742
Jiarui Xu <christinexu@Jiaruis-MacBook-Pro.local>
2024-03-07 14:22:25 -0800

    minor

--- streamlit/config.py
+++ streamlit/config.py
@@ -4,6 +4,7 @@ PICKLE_FULL_PATH = "./data/full_recipes.pkl"
 
 TFIDF_ENCODING_PATH = "models/tfidf_encodings.pkl"
 TFIDF_MODEL_PATH = "models/tfidf.pkl"
+W2V_PATH = "models/model_cbow_2.bin"
 
 
 # LINUX_PATH = "/home/jack/projects/whatscooking-deployment/"

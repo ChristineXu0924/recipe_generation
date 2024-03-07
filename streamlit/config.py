2f1027eb59d575bdfa7710fcf9b0381ee4e88864
ChristineXu0924 <100732262+ChristineXu0924@users.noreply.github.com>
2024-03-07 14:37:41 -0800

    Update config.py

--- streamlit/config.py
+++ streamlit/config.py
@@ -1,10 +1,10 @@
-RECIPES_PATH = "./data/combined_foodcom.csv"
-PARSED_PATH = "./data/df_parsed.csv"
-PICKLE_FULL_PATH = "./data/full_recipes.pkl"
+RECIPES_PATH = "./streamlit/data/combined_foodcom.csv"
+PARSED_PATH = "./streamlit/data/df_parsed.csv"
+PICKLE_FULL_PATH = "./streamlit/data/full_recipes.pkl"
 
-TFIDF_ENCODING_PATH = "models/tfidf_encodings.pkl"
-TFIDF_MODEL_PATH = "models/tfidf.pkl"
-W2V_PATH = "models/model_cbow_2.bin"
+TFIDF_ENCODING_PATH = "./streamlit/models/tfidf_encodings.pkl"
+TFIDF_MODEL_PATH = "./streamlit/models/tfidf.pkl"
+W2V_PATH = "./streamlit/models/model_cbow_2.bin"
 
 
 # LINUX_PATH = "/home/jack/projects/whatscooking-deployment/"

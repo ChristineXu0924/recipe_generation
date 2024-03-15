146f54b7311117a29d280b4c6b846538e38f3901
Jiarui Xu <christinexu@Christines-MBP.lan>
2024-03-15 02:24:09 -0700

    change key access

--- streamlit/streamlit_app.py
+++ streamlit/streamlit_app.py
@@ -166,8 +166,8 @@ with st.sidebar:
 
 
 ######################### Use input_image/image_reader instead ##########
-# openai_api_key = st.secrets.db_credentials.openai_key
-openai_api_key = st.session_state['api_key']
+openai_api_key = st.secrets.db_credentials.openai_key
+# openai_api_key = st.session_state['api_key']
 uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
 
 def image_to_base64(image: Image.Image) -> str:
